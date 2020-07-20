bce692ba5fa5574ba2752b8208ebb6a900a22b1a
Maks <panasenkomaksim@gmail.com>
2020-07-20 20:20:05 +0300

    completed homework for lesson 9

--- src/Lesson9/Main.java
+++ src/Lesson9/Main.java
@@ -3,10 +3,10 @@ package Lesson9;
 public class Main {
     public static void main(String[] args) {
 
-/*
+        System.out.println("_____________ Все методы отработают ________________");
 
         Car car1 = new Car();
-        Auto<Car> c1 = new Auto<Car>(car1);
+        Auto<Car> c1 = new Auto<>(car1);
         methodSuper(c1);
         methodExtends(c1);
 
@@ -19,34 +19,9 @@ public class Main {
         Auto<Accord> ac1 = new Auto<Accord>(accord1);
         methodSuper(ac1);
         methodExtends(ac1);
-*/
 
 
-        System.out.println("_____________Параметризация аргумента метода extends________________");
-
-
-//        Auto<Car> c2 = new Auto<>();
-//        methodSuperLimited(c2, new Car());
-//        // methodExtendsLimited(c2); - Ошибка на этапе компиляции так-как объект Car не является наследником объекта Honda
-//
-//
-//        // Код скомпилируется из-за того, что объекты попадают в диапазон ограничения Honda
-//        Auto<Honda> a2 = new Auto<>();
-//       // methodSuperLimited(a2, new Car<Honda>());
-//       // methodExtendsLimited(a2);
-//
-//        Auto<Car> ac1 = new Auto<>();
-//        methodSuperLimited(ac1, new Accord());
-//        //methodExtends(ac1);
-//    }
-//
-////    public static void methodExtends(Auto<? extends Car> auto) {
-////        auto.getAuto().get();
-////    }
-////
-////    public static void methodSuper (Auto<? super Accord> auto) {
-////        auto.setAuto();
-////    }
+        System.out.println("_____________Демонстрация ограничений аргументов________________");
 
         Car car = new Car();
         Auto<Car> c2 = new Auto<>(car);
@@ -54,25 +29,32 @@ public class Main {
         //methodExtendsLimited(c2); //- Ошибка на этапе компиляции так-как объект Car не является наследником объекта Honda
 
         Honda honda = new Honda();
-        Auto<Honda> h2= new Auto<>(honda);
+        Auto<Honda> h2 = new Auto<>(honda);
         methodSuperLimited(h2);
         methodExtendsLimited(h2); //- Компиляция будет проведена успешно
 
         Accord accord = new Accord();
-        Auto<Accord> a2= new Auto<>(accord);
-        //methodSuperLimited(a2);
-        methodExtendsLimited(a2); 
+        Auto<Accord> a2 = new Auto<>(accord);
+        //methodSuperLimited(a2); // - Ошибка компиляции, так-как Accord не является родительским классом для Honda
+        methodExtendsLimited(a2);
+
     }
 
-    public static void methodSuperLimited(Auto<? super Honda> auto) {
-       // auto.setAuto();
-    }
-
-    public static void methodExtendsLimited (Auto<? extends Honda> auto) {
+    public static void methodExtends(Auto<? extends Car> auto) {
         auto.getAuto().get();
     }
 
+    public static void methodSuper(Auto<? super Accord> auto) {
+        auto.setAuto();
+    }
 
+    public static void methodSuperLimited(Auto<? super Honda> auto) {
+        auto.setAuto();
+    }
+
+    public static void methodExtendsLimited(Auto<? extends Honda> auto) {
+        auto.getAuto().get();
+    }
 }
 
 class Car<C> {
@@ -93,6 +75,7 @@ class Accord<A> extends Honda {
     }
 }
 
+
 class Auto<T> {
 
     T auto;
@@ -108,9 +91,8 @@ class Auto<T> {
         return auto;
     }
 
-//    public void setAuto(T auto) {
-//        System.out.println("В обекте передан обект " + auto.getClass().getName());
-//        this.auto = auto;
-//    }
+    public void setAuto() {
+        System.out.println("Метод добавления авто");
+    }
 
 }
